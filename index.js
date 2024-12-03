540bc1a5cf3787d2c2e4f84e1d71ad83b0a0bb63
Jhonatan1221 <108557046+Jhonatan1221@users.noreply.github.com>
2024-12-02 20:36:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
+
+// Configurar CORS
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000"], // Permitir solo solicitudes desde el frontend local
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+    credentials: true, // Si usas cookies o autenticación basada en sesiones
   })
 );
 
+
+// Configurar puerto y middlewares
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
+// Rutas
 app.use("/api", userRouter);
 
+// Endpoint raíz
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.status(200).json({ message: "API is running...", version: "1.0.0" });
 });
+
+// Conexión a MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -32,5 +42,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.error(`Error connecting to mongo: ${error.message}`);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
   });
